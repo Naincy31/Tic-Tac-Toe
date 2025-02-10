c82245209562d22f7c705d13547eae5bb17cd7af
Naincy31 <iamnaincyrathore@gmail.com>
2025-02-10 11:08:21 +0530

    refactor: moving sound logic from App to GameSound component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import './App.css';
 import KnowGridSize from './components/KnowGridSize';
-import { useEffect } from 'react';
 import GameBoard from './components/GameBoard';
-import { playClickSound } from './utils/playClickSound';
 import GameSound from './components/GameSound';
 import { useSelector } from 'react-redux';
 
 function App() {
   const gridSize = useSelector(state => state.game.gridSize)
-  const soundVolume = useSelector(state => state.game.soundVolume)
-
-  useEffect(() => {
-    console.log(gridSize)
-  }, [gridSize])
-
-  useEffect(() => {
-    const handleClick = (event) => {
-      if (event.target.tagName === 'BUTTON') {
-        playClickSound(soundVolume)
-      }
-    }
-
-    document.addEventListener('click', handleClick)
-
-    return () => {
-      document.removeEventListener('click', handleClick)
-    }
-  }, [soundVolume])
 
   return (
     <>
--- src/components/GameSound.jsx
+++ src/components/GameSound.jsx
@@ -1,11 +1,27 @@
 import mute from '../assets/images/mute.png';
 import volume from '../assets/images/volume-up.png';
+import { useEffect } from 'react';
+import { playClickSound } from '../utils/playClickSound';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleSound } from '../redux/gameSlice';
 
 const GameSound = () => {
     const soundVolume = useSelector(state => state.game.soundVolume)
     const dispatch = useDispatch()
+
+    useEffect(() => {
+        const handleClick = (event) => {
+          if (event.target.tagName === 'BUTTON') {
+            playClickSound(soundVolume)
+          }
+        }
+    
+        document.addEventListener('click', handleClick)
+    
+        return () => {
+          document.removeEventListener('click', handleClick)
+        }
+    }, [soundVolume])
 
   return (
     <div className='sound-icon' onClick={() => dispatch(toggleSound())}>
